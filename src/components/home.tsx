6fdab1daa9d8f5e548f0fae543922680b4535d6d
Harsh <harshguptaaa17@gmail.com>
2024-10-25 01:39:45 +0530

    Add Popular Component

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,10 +5,12 @@ import { Cover } from "@/components/ui/cover";
 import { motion } from "framer-motion";
 import { FeaturesSectionDemo } from "./feature";
 import Hero from "./hero";
+import { Popular } from "./popular";
 function HomeComponent() {
   return (
     <div>
       <Hero/>
+      <Popular/>
     </div>
 
   );
