308a9a7590890106e5f5e94132eb810ba93f3b27
Harsh <harshguptaaa17@gmail.com>
2024-10-24 23:42:56 +0530

    Navbar done

---
+++ src/components/hero.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <div className="w-[80vw] h-[80vh] border-2 border-black mx-auto place-items-center mt-4 rounded-md overflow-clip">
+      <div className="absolute z-10 flex flex-col place-items-center justify-center gap-2 border-2">
+        <h1 className="text-white text-4xl font-bold italic">
+          Welcome to Vishwakarma Woodworks.
+        </h1>
+        <p className="text-white font-semibold">New in from Vishwakarma Woodworks. Traditional craftmanship.</p>
+        <button className="bg-gray-700/50 hover:bg-gray-700/70 px-6 py-3 mt-4 text-white border-2 border-black/30 rounded-md">Shop Products</button>
+      </div>
+      <div className="relative w-full h-full">
+        <Image
+          src="/Sofas/sofa1.webp"
+          layout="fill"
+          objectFit="cover"
+          alt="Sofa Image"
+        />
+      </div>
+    </div>
+  );
+}
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -4,32 +4,13 @@ import { AuroraBackground } from "./ui/aurora-background";
 import { Cover } from "@/components/ui/cover";
 import { motion } from "framer-motion";
 import { FeaturesSectionDemo } from "./feature";
+import Hero from "./hero";
 function HomeComponent() {
   return (
-    <AuroraBackground>
-      <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="relative flex flex-col gap-4 items-center justify-center px-4 pt-20"
-      >
-        <div className="text-center">
-          <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
-            Auth.js and Prisma starter template build <br /> with{" "}
-            <Cover>Next.js</Cover>
-          </h1>
-          <p className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
-            Supercharge your project with a powerful, feature-rich
-            authentication setup
-          </p>
-        </div>
-        <FeaturesSectionDemo />
-      </motion.div>
-    </AuroraBackground>
+    <div>
+      <Hero/>
+    </div>
+
   );
 }
 
