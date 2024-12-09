6acb916db397fa93bd1bc08a9d8252cc85d4ea79
mdansarijaved <javedans2003@gmail.com>
2024-12-09 10:41:50 +0530

    cors issue

--- src/lib/ikUpload.ts
+++ src/lib/ikUpload.ts
@@ -1,6 +1,6 @@
 const authenticator = async () => {
   try {
-    const response = await fetch("http://localhost:3000/api/auth");
+    const response = await fetch("/api/auth");
 
     if (!response.ok) {
       const errorText = await response.text();
